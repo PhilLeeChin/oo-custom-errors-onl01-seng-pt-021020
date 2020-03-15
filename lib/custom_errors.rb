b09740c193de924b9ebc61b9e63188d8c24f9480
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-15 23:48:49 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,13 +9,11 @@ class Person
     self.partner = person
     person.partner = self
   end
-
 end
+
+  class PartnerError < StandardError
+  end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
